9d39d2fd6ce61918aadf99a3dc13179f6af66b2f
Diego <dicolomb@microsoft.com>
2020-08-06 18:15:26 +0100

    experiments

---
+++ src/Microsoft.DotNet.Interactive.DataExplorer.js/src/interface.ts
@@ -0,0 +1,42 @@
+import ReactDOM from "react-dom";
+import { DataExplorer } from "@nteract/data-explorer";
+import React from 'react';
+
+export interface DataExplorerSettings {
+    container: HTMLDivElement,
+    data: DataProps
+}
+
+export interface DataProps {
+    schema: Schema;
+    data: Datapoint[];
+}
+export interface Schema {
+    fields: Field[];
+    pandas_version?: string;
+    primaryKey?: string[];
+}
+
+export interface Field {
+    name: string;
+    type: string;
+}
+
+export interface Datapoint {
+    [fieldName: string]: any;
+}
+
+// this type follow the schema published at https://specs.frictionlessdata.io//tabular-data-resource/
+export interface TabularDataResource {
+    schema: {
+        fields: Array<{ name: string, type: string }>,
+        primaryKey: string
+    },
+    data: Array<{
+        [key: string]: any
+    }>
+}
+
+export function createDataExplorer(settings: DataExplorerSettings) {
+    ReactDOM.render(React.createElement(DataExplorer, { data: settings.data }), settings.container);
+}
---
+++ src/Microsoft.DotNet.Interactive.DataExplorer.js/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require('path');
+const version = require('./package.json').version;
+
+// Custom webpack rules
+const rules = [
+  { test: /\.ts$/, loader: 'ts-loader' },
+  { test: /\.js$/, loader: 'source-map-loader' },
+  { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+];
+
+// Packages that shouldn't be bundled but loaded at runtime
+const externals = ['@jupyter-widgets/base'];
+
+const resolve = {
+  // Add '.ts' and '.tsx' as resolvable extensions.
+  extensions: [".webpack.js", ".web.js", ".ts", ".js"]
+};
+
+module.exports = [
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'index.js',
+      path: path.resolve(__dirname, 'dist'),
+      libraryTarget: 'umd',
+      library: 'interactiveDataExplorer',
+    },
+    module: {
+      rules: rules
+    },
+    devtool: 'source-map',
+    externals,
+    resolve,
+    optimization: {
+      minimize: true
+    }
+  },
+
+  {
+    entry: './src/index.ts',
+    output: {
+      filename: 'lib.js',
+      path: path.resolve(__dirname, 'dist'),      
+      library: 'interactiveDataExplorer',
+      libraryTarget: 'umd',
+    },
+    module: {
+      rules: rules
+    },
+    devtool: 'source-map',
+    externals,
+    resolve,
+    optimization: {
+      minimize: true
+    }
+  }
+];
